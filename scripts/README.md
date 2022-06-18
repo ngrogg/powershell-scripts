76c9e889bed3715a5b8fea093b3e7d535c4a4e36
Nicholas Grogg <ngrogg@pop-os.localdomain>
2022-06-18 11:12:01 -1000

    Created diskSpace helper script

--- scripts/README.md
+++ scripts/README.md
@@ -1,3 +1,11 @@
 # scripts 
 
 Scripts that I've written for Powershell. </br>
+
+## Scripts 
+**helloWorld**, the simple 'hello world' script to test that everything is configured and working on your system. </br>
+Usage, just run the script </br>
+
+**diskSpace**, A Powershell script to find the largest files and folders at a provided filepath.
+Takes a filepath as an argument, doesn't have to be C:\ drive. For best results run in admin powerShell prompt. </br>
+Usage, `.\diskSpace.ps1 C:\FILEPATH` </br>
---
+++ scripts/diskSpace/diskSpace.ps1
@@ -0,0 +1,61 @@
+# Powershell script to find large files and folders at a provided filepath
+# Helper script by Nicholas Grogg
+
+# Help
+if ($args[0] -match 'help'){
+	Write-Host "Help"
+	Write-Host "-----------------------------------------------"
+	Write-Host "List the largest files and folders at a given filepath"
+	Write-Host "Usage: .\diskSpace.ps1 C:\filepath"
+	Write-Host "Run as admin, needs to be able to access directories"
+	Write-Host "-----------------------------------------------"
+	Exit
+}
+
+# Pass filepath variable 
+$filepath=$args[0]
+
+# Checks for null variables 
+## Check if filepath defined 
+if (!$filepath){
+	Write-Host "A check has failed"
+	Write-Host "-----------------------------------------------"
+	$filepath = Read-Host -Prompt "filepath is not defined, please enter a filepath:"
+	Write-Host "-----------------------------------------------"
+}
+
+# If filepath STILL undefined, exit 
+if (!$filepath){
+	Write-Host "-----------------------------------------------"
+	Write-Host "filepath still undefined, re-run script with a defined filepath"
+	Write-Host "Usage: .\diskSpace.ps1 FILEPATH"
+	Write-Host "-----------------------------------------------"
+	Exit
+}
+
+# Formatting
+Write-Host "Folders using the most space at $filepath"
+Write-Host "-----------------------------------------------"
+
+# Find size of directories at filepath
+gci -force $filepath -ErrorAction SilentlyContinue | ? { $_ -is [io.directoryinfo] } | % {
+$len = 0
+gci -recurse -force $_.fullname -ErrorAction SilentlyContinue | % { $len += $_.length }
+$_.fullname, '{0:N2} GB' -f ($len / 1Gb)
+}
+
+# Formatting
+Write-Host "-----------------------------------------------"
+Write-Host "Copy/Paste relevant info for disk space tickets"
+Write-Host ""
+
+# Formatting 
+Write-Host "Files using the most space at $filepath"
+Write-Host "-----------------------------------------------"
+
+# Find files in filespath and sort them by size 
+gci $filepath -r| sort -descending -property length | select -first 10 name, @{Name="GB";Expression={[Math]::round($_.length / 1GB, 2)}} | Format-Table
+
+# Formatting 
+Write-Host "-----------------------------------------------"
+Write-Host "Copy/Paste relevant info for disk space tickets"
